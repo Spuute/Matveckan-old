03649939c0002b4202b378dc331bd1bbe994c2ce
Spuute <spuute@gmail.com>
2021-06-06 21:43:36 +0200

    Added Recipe controller and class

---
+++ Source/Backend/Controllers/RecipeController.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RecipeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Recipes(){
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Recipe(int id){
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public IActionResult Recipe(){
+            throw new NotImplementedException();
+        }
+    }
+}
