cafc28dfb4757ad13b1a435638c9f44933575313
Patric Bergkvist <70053524+Spuute@users.noreply.github.com>
2021-06-12 12:36:16 +0200

    Revert "Revert "dto""
    This reverts commit 8a3866705d92bd426f52774eb9687e92eb702077.

--- Source/Backend/Mapping/RecipeDTO.cs
+++ Source/Backend/Mapping/RecipeDTO.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using AutoMapper;
 using Backend.Models;
+using Backend.Resources;
 
 namespace Backend.Mapping
 {
@@ -7,6 +9,14 @@ namespace Backend.Mapping
     {
         public string Name { get; set; } 
         public Category Categories { get; set; }
-        public List<IngredientResource> Ingredients { get; set; }
+        public List<Ingredient> Ingredients { get; set; }
+    }
+
+    public class RecipeDTOto  : Profile
+    {
+        public RecipeDTOto() {
+            CreateMap<Recipe, RecipeDTO>();
+            CreateMap<Ingredient, RecipeDTO>();
+        }
     }
 }
