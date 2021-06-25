eec45fd5be522d6fa1baa97908808d259fdd4f43
Patric Bergkvist <70053524+Spuute@users.noreply.github.com>
2021-06-25 21:37:22 +0200

    Frontend progress

---
+++ Source/Frontend/Pages/hejsan.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Frontend.Pages
+{
+    public class hejsanModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
